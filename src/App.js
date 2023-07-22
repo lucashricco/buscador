e7e76561a7bf82c52fcd0399a4ddca8520070bec
lucashricco <silianolucas@gmail.com>
2023-07-22 16:28:35 -0300

    inclusao da api e finalização do design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi'
 import './style.css'
+import api from './services/api'
 
 function App() {
+
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({});
+
+  async function handleSearch(){
+    
+    if(input === ''){
+      alert("Digite um CEP!!!")
+      return;
+    }
+    
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput("")
+    }catch{
+      alert("Erro ao buscar o CEP! Digite novamente!")
+      setInput("")
+    }
+
+  }
+
+
   return (
     <div className="container">
         <h1 className="title">BUSCADOR DE CEP</h1>
 
         
     <div className="containerInput">
-      <input type="text" placeholder="Digite seu CEP..."></input>
-      <button className="buttonSearch"><FiSearch size={25} color='#FFF'/></button>
+      <input type="text" 
+      placeholder="Digite seu CEP..."
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      >
+
+      </input>
+      <button className="buttonSearch" onClick={handleSearch}><FiSearch size={25} color='#FFF'/></button>
     </div>
 
-    <main className="main">
-      <h2>CEP: 15600000</h2>
+    {Object.keys(cep).length > 0 && (
+        <main className="main">
+        <h2>CEP: {cep.cep}</h2>
 
-      <span>Rua Teste</span>
-      <span>Complemento Teste</span>
-      <span>Bairro Teste</span>
-      <span>Fernandópolis - SP</span>
+        <span>Logradouro: {cep.logradouro}</span>
+        <span>Complemento: {cep.complemento}</span>
+        <span>Bairro: {cep.bairro}</span>
+        <span>Cidade: {cep.localidade} - {cep.uf}</span>
 
-    </main>
+      </main>
+    )}
+
 
     </div>
     
